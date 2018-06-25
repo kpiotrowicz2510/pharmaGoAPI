6e465130d2df1d5526414e2d0757a4c1606be20c
Krzysztof Piotrowicz <krzychu2510@gmail.com>
2018-06-25 19:07:14 +0200

    Allow CORS

--- pharmaGo/Startup.cs
+++ pharmaGo/Startup.cs
@@ -29,7 +29,20 @@ namespace pharmaGo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                    });
+            });
             services.AddMvc();
+
 			services.AddEntityFrameworkSqlite().AddDbContext<DBContext>();
         }
 
@@ -41,6 +54,7 @@ namespace pharmaGo
                 app.UseDeveloperExceptionPage();
             }
 
+			app.UseCors("AllowAll");
 
             app.UseMvc();
         }
